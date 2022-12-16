cc166a5d060cdfce32231d39c0cb128d986c5c8e
Nikola <nikola.nextlab3@gmail.com>
2022-12-16 17:05:54 +0100

    New library

--- src/components/converter/converter.tsx
+++ src/components/converter/converter.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react';
 import './converter.scss';
 
+import MetaMaskSDK from '@metamask/sdk';
+import detectEthereumProvider from '@metamask/detect-provider';
+
+const metamask = new MetaMaskSDK();
+//const provider = await detectEthereumProvider();
+
 const Converter = () => {
 
   const[fromValue, setFromValue] = useState('0.0');
+
+  const getNetworkInfo = async () => {
+    console.log();
+  };
 
   return(
   <div className="converter">
@@ -13,7 +23,7 @@ const Converter = () => {
         <input type="text" placeholder='0.0'/>
       </div>
       <div className="secondPart">
-
+        <button onClick={getNetworkInfo}>Click me!</button>
       </div>
     </div>
     <div className="toTable">
--- src/utilities/deposit.ts
+++ src/utilities/deposit.ts
@@ -2,7 +2,7 @@ import { ethers, Signer } from "ethers";
 import bridgeContract from "../assets/contracts/Bridge.json";
 
 const deposit = async (bridgeAddress: string, wallet: Signer) => {
-    const gasLimit = ethers.utils.hexlify();
+    //const gasLimit = ethers.utils.hexlify();
     const gasPrice = ethers.utils.hexlify(25000000000);
     const bridge = new ethers.Contract(bridgeAddress, bridgeContract.abi, wallet);
 };
